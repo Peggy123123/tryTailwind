1a666c852cfda21eb27601ad92e452697a1b68cc
Peggy <papax1123@gmail.com>
2024-09-12 15:46:56 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //tab
-const tabElement = document.getElementById("tabs-example");
+const tabsElement = document.getElementById("tabs-example");
 
-const tabElements = [
+const tabsElements = [
   {
     id: "profile",
     triggerEl: document.getElementById("profile-tab-example"),
@@ -24,22 +24,28 @@ const tabElements = [
   },
 ];
 
-const options = {
+const tabsOptions = {
   defaultTabId: "dashboard",
 };
 
-const instanceOptions = {
+const tabsInstanceOptions = {
   id: "tabs-example",
   override: true,
 };
 
-const tab = new Tabs(tabElement, tabElements, options, instanceOptions);
+const tab = new Tabs(
+  tabsElement,
+  tabsElements,
+  tabsOptions,
+  tabsInstanceOptions
+);
 
 //在某個動作之後會指定當前tab
 setTimeout(() => {
   tab.show("profile");
 }, 3000);
 
+//點擊定tab後要做的動作
 tab.updateOnShow((id) => {
   switch (id._activeTab.id) {
     case "profile":
@@ -51,16 +57,61 @@ tab.updateOnShow((id) => {
   }
 });
 
-//dark模式
-const lightBtn = document.getElementById("light-mode");
-const darkBtn = document.getElementById("dark-mode");
-lightBtn.addEventListener("click", () => {
-  document.documentElement.classList.remove("dark");
-  document.documentElement.classList.add("bg-white");
-  document.documentElement.classList.remove("bg-black");
+//carousel
+const carouselElement = document.getElementById("carousel-example");
+
+const carouselItems = [
+  {
+    position: 0,
+    el: document.getElementById("carousel-item-1"),
+  },
+  {
+    position: 1,
+    el: document.getElementById("carousel-item-2"),
+  },
+];
+
+const carouselOptions = {
+  defaultPosition: 1,
+  interval: 3000,
+
+  indicators: {
+    activeClasses: "bg-white dark:bg-gray-800",
+    inactiveClasses:
+      "bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800",
+    items: [
+      {
+        position: 0,
+        el: document.getElementById("carousel-indicator-1"),
+      },
+      {
+        position: 1,
+        el: document.getElementById("carousel-indicator-2"),
+      },
+    ],
+  },
+};
+
+const carouselInstanceOptions = {
+  id: "carousel-example",
+};
+
+const carousel = new Carousel(
+  carouselElement,
+  carouselItems,
+  carouselOptions,
+  carouselInstanceOptions
+);
+
+const prevBtn = document.getElementById("data-carousel-prev");
+const nextBtn = document.getElementById("data-carousel-next");
+
+prevBtn.addEventListener("click", () => {
+  carousel.prev();
 });
-darkBtn.addEventListener("click", () => {
-  document.documentElement.classList.add("dark");
-  document.documentElement.classList.add("bg-black");
-  document.documentElement.classList.remove("bg-white");
+
+nextBtn.addEventListener("click", () => {
+  carousel.next();
 });
+
+// carousel.cycle();
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#f5f5f5",
+          200: "#e4e3e1",
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
   darkMode: "class",
